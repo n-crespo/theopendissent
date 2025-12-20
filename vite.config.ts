b446262ba57392632efc006126e81438cc9a6419
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-12-20 01:25:24 -0500

    build: move favicon and other resources to public/

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  // ensures assets are served from the root
+  base: "/",
   // adds support for importing svgs as strings
   assetsInclude: ["**/*.svg"],
 });
