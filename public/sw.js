9e77891467182ae1ce9ab82d62e1d1b225710c90
Nicolas Crespo <nicolasecrespo@gmail.com>
2026-01-01 01:14:39 -0500

    chore(sw): control open clients expliclty

--- public/sw.js
+++ public/sw.js
@@ -2,6 +2,12 @@ self.addEventListener("install", (event) => {
   self.skipWaiting();
 });
 
+self.addEventListener("activate", (event) => {
+  // immediately control any open clients
+  event.waitUntil(self.clients.claim());
+});
+
 self.addEventListener("fetch", (event) => {
   // basic fetch listener for pwa compliance
+  event.respondWith(fetch(event.request));
 });
