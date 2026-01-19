4a774f2712a17b4bd409ef975fe26382a4824f7c
Nicolas Crespo <nicolasecrespo@gmail.com>
2026-01-18 22:55:19 -0800

    perf: improve code chunking to avoid heavy download

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,28 @@ export default defineConfig({
   base: "/",
   // adds support for importing svgs as strings
   assetsInclude: ["**/*.svg"],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Put all Firebase code in its own file
+          if (id.includes("node_modules/firebase")) {
+            return "firebase";
+          }
+          // Put React and Router in their own file
+          if (
+            id.includes("node_modules/react") ||
+            id.includes("node_modules/react-dom") ||
+            id.includes("node_modules/react-router")
+          ) {
+            return "react-vendor";
+          }
+          // Put Framer Motion in its own file
+          if (id.includes("node_modules/framer-motion")) {
+            return "framer";
+          }
+        },
+      },
+    },
+  },
 });
