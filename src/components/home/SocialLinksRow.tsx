ecddc73654c552c9b4646885e62c422b52c8bfe4
Nicolas Crespo <nicolasecrespo@gmail.com>
2026-03-05 23:22:11 -0800

    style(socials): remove grayscale

--- src/components/home/SocialLinksRow.tsx
+++ src/components/home/SocialLinksRow.tsx
@@ -4,25 +4,25 @@ export const SocialLinksRow = () => {
       name: "Spotify",
       url: "https://open.spotify.com/show/471WfoA8k9zORQPQbLynw2?si=81fb44fe7dd945bf",
       img: "/spotify_logo.png",
-      brandColor: "hover:drop-shadow-[0_0_8px_rgba(29,185,84,0.4)]",
+      brandColor: "[0_0_8px_rgba(29,185,84,0.4)]",
     },
     {
       name: "Apple Podcasts",
       url: "https://podcasts.apple.com/hr/podcast/the-open-dissent/id1860727185",
       img: "/apple_podcasts_logo.png",
-      brandColor: "hover:drop-shadow-[0_0_8px_rgba(135,46,196,0.4)]",
+      brandColor: "[0_0_8px_rgba(135,46,196,0.4)]",
     },
     {
       name: "Instagram",
       url: "https://www.instagram.com/theopendissent/",
       img: "/instagram_logo.png",
-      brandColor: "hover:drop-shadow-[0_0_8px_rgba(237,49,126,0.4)]",
+      brandColor: "[0_0_8px_rgba(237,49,126,0.4)]",
     },
     {
       name: "LinkedIn",
       url: "https://www.linkedin.com/company/the-open-dissent",
       img: "/linkedin_logo.png",
-      brandColor: "hover:drop-shadow-[0_0_8px_rgba(10,102,194,0.4)]",
+      brandColor: "[0_0_8px_rgba(10,102,194,0.4)]",
     },
   ];
 
@@ -40,7 +40,7 @@ export const SocialLinksRow = () => {
           <img
             src={platform.img}
             alt={platform.name}
-            className={`h-6 w-6 object-contain grayscale transition-all duration-300 ease-in-out group-hover:grayscale-0 ${platform.brandColor}`}
+            className={`h-6 w-6 object-contain transition-all duration-300 ease-in-out ${platform.brandColor}`}
           />
         </a>
       ))}
