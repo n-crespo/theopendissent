4ce8d6455151c66f2f8b159bf65a3e98c5b93da6
Nicolas Crespo <nicolasecrespo@gmail.com>
2026-03-30 13:01:18 -0700

    feat(seo): add twitter tags for description/image/title

--- src/components/ui/Seo.tsx
+++ src/components/ui/Seo.tsx
@@ -1,4 +1,3 @@
-// src/components/ui/Seo.tsx
 import { Helmet } from "react-helmet-async";
 import { useLocation } from "react-router-dom";
 
@@ -9,24 +8,38 @@ interface SeoProps {
 
 export const SEO = ({ title, description }: SeoProps) => {
   const location = useLocation();
-  const baseUrl = "https://theopendissent.com";
+  const baseUrl = "https://www.theopendissent.com";
   // remove query params for the canonical link
   const canonicalUrl = `${baseUrl}${location.pathname}`;
 
   const siteName = "The Open Dissent";
   const fullTitle = title ? `${title} - ${siteName}` : siteName;
   const defaultDesc = "Join the debate on The Open Dissent.";
+  const desc = description || defaultDesc;
 
   return (
     <Helmet>
       <title>{fullTitle}</title>
+      <meta name="description" content={desc} />
       <link rel="canonical" href={canonicalUrl} />
-      <meta name="description" content={description || defaultDesc} />
-      {/* open graph / social media tags */}
+
+      <meta name="twitter:title" content={fullTitle} />
+      <meta name="twitter:description" content={desc} />
+      <meta
+        name="twitter:image"
+        content="https://theopendissent.com/og-image.png"
+      />
+      <meta property="twitter:domain" content="theopendissent.com" />
+      <meta property="twitter:url" content={canonicalUrl} />
+
       <meta property="og:title" content={fullTitle} />
-      <meta property="og:description" content={description || defaultDesc} />
+      <meta property="og:description" content={desc} />
       <meta property="og:type" content="website" />
       <meta property="og:url" content={canonicalUrl} />
+      <meta
+        property="og:image"
+        content="https://theopendissent.com/og-image.png"
+      />
     </Helmet>
   );
 };
