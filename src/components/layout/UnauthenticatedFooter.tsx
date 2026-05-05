618605a0126191f5b79ea536d09ae41d6e1c96e6
Nicolas Crespo <nicolasecrespo@gmail.com>
2026-05-05 16:09:20 -0700

    chore: increase y padding in sign in footer

--- src/components/layout/UnauthenticatedFooter.tsx
+++ src/components/layout/UnauthenticatedFooter.tsx
@@ -27,7 +27,7 @@ export const UnauthenticatedFooter = () => {
 
   return (
     <div className="fixed bottom-0 left-0 right-0 z-50 bg-logo-offwhite border-t border-slate-200 shadow-2xl transform-gpu">
-      <div className="mx-auto max-w-7xl px-4 py-3 sm:py-4 flex flex-col sm:flex-row items-center justify-between gap-3">
+      <div className="mx-auto max-w-7xl px-4 py-5 sm:py-4 flex flex-col sm:flex-row items-center justify-between gap-3">
         <div className="flex flex-col">
           <p className="text-slate-900 text-md font-semibold sm:text-base leading-tight">
             Sign in with your UCLA email to post or reply!
